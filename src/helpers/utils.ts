3ad7446bb9bdc7ba7a47b75b1934fd5b17760589
Muhammad Khurram <mkhurram@BARITECHSOL.COM>
2024-06-20 19:56:48 +0500

    registration method implemented

---
+++ src/helpers/utils.ts
@@ -0,0 +1,33 @@
+import { ErrorDetail, UnauthorizedError } from "@shipengine/connect-runtime";
+import { HttpStatusCode } from "axios";
+
+
+export const getCustomError = (err: any) => {
+    const customError = err?.Error;
+    if (customError === "Access Denied") {
+        throw new UnauthorizedError('Error from Carrier Api', [
+            {
+                errorCode: err.ErrorLevel,
+                message: "Access denied"
+            }
+
+        ]);
+    }
+};
+
+export const HandlesError = (error: any) => {
+    const errorCode: ErrorDetail[] = [];
+    if (error?.details) {
+        errorCode.push(
+            {
+                errorCode: error.details[0].errorCode,
+                message: error.details[0].message
+            }
+        )
+
+    }
+
+    if (error.statusCode === HttpStatusCode.Unauthorized) {
+        throw new UnauthorizedError("Message From Carrier Api", errorCode);
+    }
+}
--- src/methods/register/register.ts
+++ src/methods/register/register.ts
@@ -2,20 +2,24 @@ import { RegisterRequest, RegisterResponse } from "@shipengine/connect-carrier-a
 import { CarrierOperation } from "../../helpers/constants";
 import { validate } from "./validate";
 import { mapRequest } from "./map-request";
-import { BadRequestError } from "@shipengine/connect-runtime";
 import { ProcessRequest } from "../../api/api-communicator";
 import { mapResponse } from "./map-response";
 import { InternalReqRegister } from "../../helpers/internal-models";
+import { HttpStatusCode, UnauthorizedError } from "@shipengine/connect-runtime";
+
 
 export const Register = async (request: RegisterRequest): Promise<RegisterResponse> => {
     const registration_info = <InternalReqRegister>request.registration_info;
     validate(registration_info);
     try {
         const thirdParty = mapRequest(registration_info);
-        await ProcessRequest(thirdParty, CarrierOperation.Register); 
-        
-    } catch (error) {
+        await ProcessRequest(thirdParty, CarrierOperation.Register);
 
+    } catch (error) {
+        if (error.statusCode === HttpStatusCode.UnAuthorized) {
+            throw new UnauthorizedError(error.details[0].message);
+        }   
     }
+
     return mapResponse(registration_info);
 }
